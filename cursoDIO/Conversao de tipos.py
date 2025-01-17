5804e47f609f7c56f42ff873400a2bb1d995824b
SamanthaSgl <samanthasasa1415@gmai.com>
2025-01-16 23:30:10 -0300

    l

---
+++ cursoDIO/Conversao de tipos.py
@@ -0,0 +1,17 @@
+preco=20
+print(preco)
+#retorna 20
+
+preco = float(preco)
+print(preco)
+
+#retorna 20.0
+
+preco2= 10.3
+print(preco2)
+
+#retorna 10.3
+
+preco2=int(preco2)
+print(preco2)
+#retorna 10
