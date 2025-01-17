63381750ccbdd9fc98a2c479a0795cb186696e4f
SamanthaSgl <samanthasasa1415@gmai.com>
2025-01-17 18:59:48 -0300

    Estruturas condicionais e de repetição

---
+++ cursoDIO/Estruturas condicionais e de repetição/Identação e blocos.py
@@ -0,0 +1,5 @@
+# Identação é para manter o codigo fonte mais legivel e manutenível
+#Especialmente no Python tem que haver essa identação por não ter ";" ou "{}"
+#para fechar o codigo , ou palavras reservadas para fazer o mesmo
+
+
